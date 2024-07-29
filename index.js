1941105c8934959cfa5181812dfb85d4f42cb878
Kgaladi Lelo Mohlabe <kgaladipmohlabe@gmail.com>
2024-07-29 15:51:53 +0200

    Add game logic for black jack game

--- index.js
+++ index.js
@@ -5,7 +5,41 @@
 
 let firstCard = 6
 let secondCard = 9
-
 let sum = firstCard + secondCard
 
-console.log(sum)
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("#sum-el")
+let cardsEl = document.getElementById("#cards-el")
+
+// Defining game logic //
+
+function startGame() {
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum 
+    if (sum <= 20){ 
+        message= "Do you want to draw a new card"
+    } else if (sum === 21 ) {
+        message= "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message= "You're out of the game!"
+        isAlive = false
+    }
+}
+
+function newCard() {
+    console.log("Drawing a new card from the deck!")
+    let card = 7
+    sum += card
+    renderGame()
+}
+
+
+messageEl.textContent = message
