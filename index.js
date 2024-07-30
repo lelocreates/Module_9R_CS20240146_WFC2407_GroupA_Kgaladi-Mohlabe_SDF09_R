3c1cb15e6c44b7c4d70b0427afbbc7bdbe469b90
Kgaladi Lelo Mohlabe <kgaladipmohlabe@gmail.com>
2024-07-30 20:08:34 +0200

    Add for loop to render cards array

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 // Card number variables 
 let firstCard = 6
 let secondCard = 9
+let cards = [firstCard, secondCard] 
 let sum = firstCard + secondCard
 
 // Variables that determine the state of the game 
@@ -18,14 +19,20 @@ let sumEl = document.getElementById("#sum-el")
 let cardsEl = document.getElementById("#cards-el")
 
 // Defining game logic with startGame and renderGame function //
-
+// function thaat starts a new game 
 function startGame() {
     renderGame()
 }
 
+// funtion that renders the game when start game or new card is clicked 
 function renderGame() {
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    cardsEl.textContent = "Cards: " 
     sumEl.textContent = "Sum: " + sum 
+
+    // for loop to render out all the cards in the array the time
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
 
     // if less than or equal to 20 -> "Do you want to draw a new card? 🙂"
     // else if exactly 21 -> "Wohoo! You've got Blackjack! 🥳"
@@ -47,6 +54,7 @@ function newCard() {
     console.log("Drawing a new card from the deck!")
     let card = 7
     sum += card
+    cards.push(card) //pushes cards to cards array
     renderGame()
 }
 
